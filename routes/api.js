5f1c113a5b6435e57da71b349238591dcdc4c487
Replit user <>
2023-12-27 05:03:22 +0000

    add validation and update route

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,20 @@
-'use strict';
-
-const Translator = require('../components/translator.js');
+"use strict";
+const { body, check } = require("express-validator");
+const Translator = require("../components/translator.js");
+const { validate } = require("../components/validate.js");
+import "regenerator-runtime/runtime";
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app
+    .route("/api/translate")
+    .post(
+      validate([body("locale").notEmpty(), body("text").exists()]),
+      (req, res) => {
+        const { text, locale } = req.body;
+        let translation = translator.translate(text, locale);
+        res.json(translation);
+      },
+    );
 };
