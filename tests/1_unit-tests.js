862419f55787cbe0b06572935a911a0a557ec22c
Replit user <>
2023-12-27 05:04:55 +0000

    settig up tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,8 +1,85 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
-suite('Unit Tests', () => {
-
+suite("Unit Tests", () => {
+  suite("Translate American to British English", () => {
+    test("Mangoes are my favorite fruit", (done) => {
+      done();
+    });
+    test("I ate yogurt for breakfast.", (done) => {
+      done();
+    });
+    test("We had a party at my friend's condo.", (done) => {
+      done();
+    });
+    test("Can you toss this in the trashcan for me?", (done) => {
+      done();
+    });
+    test("The parking lot was full.", (done) => {
+      done();
+    });
+    test("Like a high tech Rube Goldberg machine.", (done) => {
+      done();
+    });
+    test("To play hooky means to skip class or work.", (done) => {
+      done();
+    });
+    test("No Mr. Bond, I expect you to die.", (done) => {
+      done();
+    });
+    test("Dr. Grosh will see you now.", (done) => {
+      done();
+    });
+    test("Lunch is at 12:15 today.", (done) => {
+      done();
+    });
+  });
+  suite("Translate British English to American", () => {
+    test("We watched the footie match for a while.", (done) => {
+      done();
+    });
+    test("Paracetamol takes up to an hour to work.", (done) => {
+      done();
+    });
+    test("First, caramelise the onions.", (done) => {
+      done();
+    });
+    test("I spent the bank holiday at the funfair.", (done) => {
+      done();
+    });
+    test("I had a bicky then went to the chippy.", (done) => {
+      done();
+    });
+    test("I've just got bits and bobs in my bum bag.", (done) => {
+      done();
+    });
+    test("The car boot sale at Boxted Airfield was called off.", (done) => {
+      done();
+    });
+    test("Have you met Mrs Kalyani?", (done) => {
+      done();
+    });
+    test("Prof Joyner of King's College, London.", (done) => {
+      done();
+    });
+    test("Tea time is usually around 4 or 4.30.", (done) => {
+      done();
+    });
+  });
+  suite("Highlight translation in", () => {
+    test("Mangoes are my favorite fruit.", (done) => {
+      done();
+    });
+    test("I ate yogurt for breakfast.", (done) => {
+      done();
+    });
+    test("We watched the footie match for a while.", (done) => {
+      done();
+    });
+    test("Paracetamol takes up to an hour to work.", (done) => {
+      done();
+    });
+  });
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,12 +1,83 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 const assert = chai.assert;
-const server = require('../server.js');
+const server = require("../server.js");
 
 chai.use(chaiHttp);
+const requester = chai.request(server).keepOpen();
+let Translator = require("../components/translator.js");
 
-let Translator = require('../components/translator.js');
-
-suite('Functional Tests', () => {
-
+suite("Functional Tests", () => {
+  suite("POST request to /api/translate -> Translation with", () => {
+    test("text and locale fields:", (done) => {
+      done();
+    });
+    test("text and invalid locale field:", (done) => {
+      requester
+        .post("/api/translate")
+        .send({ text: "here we are", locale: "spanish-to-french" })
+        .end(function (err, res) {
+          if (err) {
+            console.error({ error: err });
+            done(err);
+          }
+          assert.deepEqual(res.body, {
+            error: "Invalid value for locale field",
+          });
+          assert.equal(res.status, 200);
+          done();
+        });
+    });
+    test("missing text field:", (done) => {
+      requester
+        .post("/api/translate")
+        .send({ locale: "american-to-british" })
+        .end(function (err, res) {
+          if (err) {
+            console.error({ error: err });
+            done(err);
+          }
+          assert.deepEqual(res.body, {
+            error: "Required field(s) missing",
+          });
+          assert.equal(res.status, 200);
+          done();
+        });
+    });
+    test("missing locale field:", (done) => {
+      requester
+        .post("/api/translate")
+        .send({ text: "Mangoes are my favorite fruit." })
+        .end(function (err, res) {
+          if (err) {
+            console.error({ error: err });
+            done(err);
+          }
+          assert.deepEqual(res.body, {
+            error: "Required field(s) missing",
+          });
+          assert.equal(res.status, 200);
+          done();
+        });
+    });
+    test("empty text:", (done) => {
+      requester
+        .post("/api/translate")
+        .send({ text: "", locale: "american-to-british" })
+        .end(function (err, res) {
+          if (err) {
+            console.error({ error: err });
+            done(err);
+          }
+          assert.deepEqual(res.body, {
+            error: "No text to translate",
+          });
+          assert.equal(res.status, 200);
+          done();
+        });
+    });
+    test("text that needs no translation:", (done) => {
+      done();
+    });
+  });
 });
